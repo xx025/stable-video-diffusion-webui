5c47ba0ef3f3020a676bf2eee2ead217f8c96838
zkpk <n@>
2023-11-28 13:05:59 +0000

    修正图片剪裁

--- img_tool.py
+++ img_tool.py
@@ -1,22 +1,29 @@
 from PIL import Image
 import os
+
+
 def auto_resize_image(input_path, output_path, max_width=1024, max_height=600):
     # 打开图片
     original_image = Image.open(input_path)
     # 获取原始图片的宽和高
     original_width, original_height = original_image.size
-    # 计算缩放比例
-    width_ratio = max_width / original_width
-    height_ratio = max_height / original_height
-    # 选择较小的比例进行缩放，以保持纵横比
-    min_ratio = min(width_ratio, height_ratio)
-    # 计算新的宽和高
-    new_width = int(original_width * min_ratio)
-    new_height = int(original_height * min_ratio)
-    # 缩放图片
-    resized_image = original_image.resize((new_width, new_height))
-    # 保存缩放后的图片
-    resized_image.save(output_path)
+
+    if original_width <= max_width and original_height <= max_height:
+        # 如果图片尺寸小于限定值，则直接保存原图
+        original_image.save(output_path)
+    else:
+        # 计算缩放比例
+        width_ratio = max_width / original_width
+        height_ratio = max_height / original_height
+        # 选择较小的比例进行缩放，以保持纵横比
+        min_ratio = min(width_ratio, height_ratio)
+        # 计算新的宽和高
+        new_width = int(original_width * min_ratio)
+        new_height = int(original_height * min_ratio)
+        # 缩放图片
+        resized_image = original_image.resize((new_width, new_height))
+        # 保存缩放后的图片
+        resized_image.save(output_path)
 
 def crop_center(image_path, output_path, target_width, target_height):
     # 打开图像
